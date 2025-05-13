dbc34027bd7a9d4be05b968cd7349a5b6f9bbe9d
LukaszPachucki <pachucki.lukasz97@gmail.com>
2025-05-13 16:24:35 +0200

    Log added

--- strategies/div.py
+++ strategies/div.py
@@ -3,4 +3,6 @@ from strategies.base import Operation
 
 class DivStrategy(Operation):
     def execute(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError("Błąd: Nie możesz dzielić przez 0")
         return a / b
