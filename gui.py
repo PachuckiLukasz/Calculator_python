662f3f32b1ae2173255be29f050e5a12e29cd56c
LukaszPachucki <pachucki.lukasz97@gmail.com>
2025-05-19 17:59:03 +0200

    m

--- gui.py
+++ gui.py
@@ -5,7 +5,6 @@ from Calculator import Calculator
 calc = Calculator()
 
 
-
 def parse_input(text: str):
     operators = ['+', '-', '*', '/', '%', '^']
     symbol_to_strategy = {
@@ -25,19 +24,23 @@ def parse_input(text: str):
         'pow': 'P'
     }
     for operator in operators:
-        if operator in text:
-            print("znaleziony operator: ", operator)
-            a_str, b_str = text.split(operator)
-            print("Lewa liczba:", a_str, "\nPrawa liczba:", b_str)
-            a = float(a_str)
-            b = float(b_str)
-            print("A float:", a, "B float:", b)
-            operation_name = symbol_to_strategy[operator]
-            calc.operator = strategy_to_symbol[operation_name]
-            calc.first_num = a
-            calc.second_num = b
-            result = calc.perform_operation()
-            print("Wynik:", result)
+        try:
+            if operator in text:
+                print("znaleziony operator: ", operator)
+                a_str, b_str = text.split(operator)
+                print("Lewa liczba:", a_str, "\nPrawa liczba:", b_str)
+                a = float(a_str)
+                b = float(b_str)
+                print("A float:", a, "\nB float:", b)
+                operation_name = symbol_to_strategy[operator]
+                calc.operator = strategy_to_symbol[operation_name]
+                calc.first_num = a
+                calc.second_num = b
+                result = calc.perform_operation()
+                label.config(text=f"Wynik: {result}")
+                print("Wynik:", result)
+        except Exception:
+                label.config(text= "Błąd - nieprawidłowe dane")
 
 def on_click():
     text = entry.get()
@@ -54,6 +57,7 @@ entry.pack()
 count = Button(frame, text="Oblicz: ", command=on_click)
 count.pack()
 
-# parse_input("5*7")
+label = Label(root, text="")
+label.pack()
 
 root.mainloop()
